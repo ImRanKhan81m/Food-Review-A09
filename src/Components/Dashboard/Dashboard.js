26a8afff9f2c935b3ac49e596a86c0d48ce541c0
Imran Hossen <freelancerimran81@gmail.com>
2022-04-04 01:04:46 +0600

    recharts updated

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,44 +1,45 @@
 
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
-import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './Dashboard.css'
 
 const Dashboard = () => {
 
     const data = [
         {
             month: "Mar",
-            investment: 100000,
+            investment: 1000,
             sell: 241,
             revenue: 10401,
         },
         {
             month: "Apr",
-            investment: 200000,
+            investment: 2000,
             sell: 423,
             revenue: 24500,
         },
         {
             month: "May",
-            investment: 500000,
+            investment: 5000,
             sell: 726,
             revenue: 67010,
         },
         {
             month: "Jun",
-            investment: 500000,
+            investment: 5000,
             sell: 529,
             revenue: 40405,
         },
         {
             month: "Jul",
-            investment: 600000,
+            investment: 6000,
             sell: 601,
             revenue: 50900,
         },
         {
             month: "Aug",
-            investment: 700000,
+            investment: 7000,
             sell: 670,
             revenue: 61000,
         },
@@ -48,27 +49,65 @@ const Dashboard = () => {
 
             <div className="container">
                 <div className="chart-container py-5 mt-3">
-                    <Row>
-                        <Col >
-                            <h2 className='text-center mb-5'>Investment VS Revenue </h2>
-                            <LineChart width={600} height={400} data={data}>
-                                <Line dataKey={'investment'}></Line>
-                                <Line dataKey={'sell'}></Line>
-                                <XAxis dataKey={'month'}></XAxis>
-                                <Tooltip/>
-                                <YAxis></YAxis>
-                            </LineChart>
-                        </Col>
-                        <Col >
-                            <h2 className='text-center mb-5'>Investment VS Revenue </h2>
-                            <LineChart width={600} height={400} data={data}>
-                                <Line dataKey={'investment'}></Line>
-                                <Line dataKey={'sell'} stroke={'green'}></Line>
-                                <XAxis dataKey={'month'}></XAxis>
-                                <YAxis></YAxis>
-                            </LineChart>
-                        </Col>
-                    </Row>
+                    <div className='mb-5 pb-5'>
+                        <Row>
+                            <Col >
+                                <h2 className='text-center mb-5'>Investment VS Revenue </h2>
+                                <LineChart width={600} height={400} data={data}>
+                                    <Line type={'monotone'} dataKey={'investment'}></Line>
+                                    <Line type={'monotone'} dataKey={'sell'} stroke='green'></Line>
+                                    <XAxis dataKey={'month'}></XAxis>
+                                    <Tooltip />
+                                    <YAxis></YAxis>
+                                </LineChart>
+                            </Col>
+                            <Col >
+                                <h2 className='text-center mb-5'>Investment VS Revenue </h2>
+                                <AreaChart
+                                    width={600}
+                                    height={400}
+                                    data={data}
+                                    margin={{
+                                        top: 10,
+                                        right: 30,
+                                        left: 0,
+                                        bottom: 0,
+                                    }}
+                                >
+                                    <CartesianGrid strokeDasharray="3 3" />
+                                    <XAxis dataKey="month" />
+                                    <YAxis />
+                                    <Tooltip />
+                                    <Area type="monotone" dataKey="investment" stroke="#8884d8" fill="#8884d8" />
+                                    <Area type="monotone" dataKey="sell" stroke="green" fill="green" />
+                                </AreaChart>
+                            </Col>
+                        </Row>
+                    </div>
+                    <div>
+                        <Row>
+                            <Col >
+                                <h2 className='text-center mb-5'>Investment VS Revenue </h2>
+                                <LineChart width={600} height={400} data={data}>
+                                    <Line type={'monotone'} dataKey={'investment'}></Line>
+                                    <Line type={'monotone'} dataKey={'sell'} stroke='green'></Line>
+                                    <XAxis dataKey={'month'}></XAxis>
+                                    <Tooltip />
+                                    <YAxis></YAxis>
+                                </LineChart>
+                            </Col>
+                            <Col >
+                                <h2 className='text-center mb-5'>Investment VS Revenue </h2>
+                                <LineChart width={600} height={400} data={data}>
+                                    <Line type={'monotone'} dataKey={'investment'}></Line>
+                                    <Line type={'monotone'} dataKey={'sell'} stroke='green'></Line>
+                                    <XAxis dataKey={'month'}></XAxis>
+                                    <Tooltip />
+                                    <YAxis></YAxis>
+                                </LineChart>
+                            </Col>
+                        </Row>
+                    </div>
                 </div>
             </div>
         </div>
